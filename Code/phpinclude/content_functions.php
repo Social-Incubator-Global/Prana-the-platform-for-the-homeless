95338d758bd7c1c43c60161f2364e615bd374f79
Oscar Tark <oscartark91@gmail.com>
2017-10-01 19:26:59 +0200

    Results.php & results.css changes
    Functions and front end elements have been upgraded for the results
    page.

--- Code/phpinclude/content_functions.php
+++ Code/phpinclude/content_functions.php
@@ -156,7 +156,18 @@ function search()
     echo("<script>document.title='Prana '+dl_r(110)+' ".$URLfilter_result[3]."  : ".$URLfilter_result[1]."'</script>");
     while($row = $result->fetch_assoc())
     {
-        echo("<div id='' style='height:300px; width:437px; background-color: rgb(220,220,220);'><input type='button' value='".$row["name"]."' id='butttitle_".$row["id"]."' style='width:100%; height:32px;'></input><br><br><div id='info_".row["id"]."'>Venue Type:<br>Address:<br>Phone:<br>Email:<br>Venue Allows/Venue Type:<br>Additional services available:</div></div><br><br>");
+        $query2 = "SELECT image_path FROM organizations_venues WHERE id=".$row["organization_venue_address"];
+        $result2 = query_($query2);
+        $img_path = "";
+        
+        //IS THERE A BETTER WAY TO GET JUST ONE LINE?
+        //GETS IMAGE PATH
+        while($row2 = $result2->fetch_assoc())
+        {
+            $img_path = $row2["image_path"];
+        }
+        
+        echo("<div class='result_box_all' id='resultbox_".$row["id"]."'><div id='content_box_header_1' style='font-family: \"Arial\", regular; color: white; font-size: 16px; background-color: rgb(9, 103, 126); width: 100%; height: 37px;'><div id='title_1' style='float: left; margin-left:2%; margin-top:4%;'>Test Venue</div></div><div id='result_image' class='result_image_div'><img class='result_image' src='../Assets/Images/def_none.png' id='img_". $row["id"] ."' style='float:left;' width=100%; height=100%;></div><br><div id='info_".row["id"]."' style='margin-top: 70%;height: 50px;margin-left: 20px;'>Venue Type:<br>Address:<br>Phone:<br>Email:<br>Venue Allows/Venue Type:<br>Additional services available:</div></div><script>document.getElementById('img_". $row["id"] ."').src = '". $img_path ."';</script>");
     }
     return;
 }
