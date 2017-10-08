b21c468bf76a1e5a15b9546cbe3608cc634f7276
nkear003 <nkearney757@gmail.com>
2017-10-08 16:44:31 +0200

    changing location of db data

--- code/views/partials/sql.blade.php
+++ code/views/partials/sql.blade.php
@@ -22,7 +22,21 @@
     along with Prana-deutschland.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-include 'connect.php'; 
+@include('view')
+
+function query_($quer)
+{
+    $link = new mysqli($hostname, $username, $password, $dbname)
+        or die("MYSQL: Unable to connect to the specific host.\n[END]\n".mysql_error());
+    $link->set_charset('utf8_swedish_ci');
+    if ($link->connect_errno)
+    {
+      echo "Failed to connect to DB: " . $link->connect_errno;
+    } //else { print 'connected'; }
+    $result = $link->query($quer) or die("".$link->error.'Query error');
+    $link->close();
+    return $result;
+}
 
 function build_query_string($URLfilter_result, $Type_)
 {
