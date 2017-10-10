f2edb806a93f2c5b2041dd4df03e04c5d670daa4
nkear003 <nkearney757@gmail.com>
2017-10-10 16:00:20 +0200

    woring on top_banner

--- code/views/pages/home.blade.php
+++ code/views/pages/home.blade.php
@@ -13,18 +13,59 @@
     var uname = localStorage.getItem("uname");
 </script>
 
-{{-- center --}}
 <div class="container-fluid" id="content">
     <div class="row">
         <div class="col">
-            @include('partials._top_banner') {{-- top_menu("home0") --}}
+            {{-- top_menu("home0") --}}
+            {{-- @include('partials._top_banner') --}}
+            {{-- top banner --}}
+            <div id="top_banner" style="" >
+                <a href="{{url('home')}}">
+                    <div id="logo">
+                        <img src="{{BASE_URL}}/assets/images/logo/prana_logo_tool.png">
+                    </div>
+                </a>
+                {{-- code snippets[0] --}}
+                @include('partials._language_select')
+                {{-- dl_d(code_snippets[7]); --}}
+                @include('partials._code_snippets_7')
+                {{-- dl_d(code_snippets[4]); --}}
+                @include('partials._emergency_button')
+                <div id="uacnt" style="">
+                    <a style="color: white;" href="javascript: if(session==1){redirect('profile');}else{redirect('login');}" id="login_text" href="#">
+                        <script>
+                            if(session==1){document.write(def_lang[35]); }else if (session!=1){ document.write(def_lang[0]); if(session!=1){document.write} }
+                        </script>
+                    </a>
+                    <script>
+                        if(session==0)
+                        {
+                            document.write(" | ");
+                        }
+                    </script>
+                    <a style="color: white;" href="javascript:redirect(\'signup\');">
+                        <script>
+                            if(session==0){document.write(def_lang[6]);}
+                        </script>
+                    </a>
+                    <a href="javascript:redirect(\'logout\');" id="login_text" href="#" style="color: white;">
+                        <script>
+                            if(session==1){document.write(" | " + def_lang[1]); }
+                        </script>
+                    </a>
+                    <a href="javascript:login_form()" id="login_text" href="#"></a>
+                </div>
+            </div>
+
+            {{-- end top banner --}}
         </div>
     </div>
     <div class="row">
         <div class="col">
         </div>
     </div>
-    @include('partials._dashboard_home') {{-- dashboard("home") --}}
+    {{-- dashboard("home") --}}
+    @include('partials._dashboard_home')
 </div>
 
 <br><br><br><br><br><br>
