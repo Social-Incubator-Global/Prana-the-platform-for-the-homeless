d5422f5bace45583fcce23b1de6fa70860d8d421
nkear003 <nkearney757@gmail.com>
2017-10-10 16:27:07 +0200

    brand image in navbar

--- code/views/pages/home.blade.php
+++ code/views/pages/home.blade.php
@@ -18,7 +18,9 @@
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="{{url('home')}}">
+            <img src="{{asset('/images/logo/prana_logo_tool.png')}}" alt="">
+        </a>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
