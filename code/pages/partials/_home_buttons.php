3009dcf7256f5c0cf7a98d0160e9e9ff25653bd9
nkear003 <nkearney757@gmail.com>
2017-10-25 20:21:02 +0200

    init

---
+++ code/pages/partials/_home_buttons.php
@@ -0,0 +1,47 @@
+<div id="home_buttons" class="row d-flex justify-content-center mt-5">
+    <ul class="list-inline">
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content', 'food', '+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/icon_food.png" class="icon">
+                <script> dl_r(19);</script>
+            </a>
+        </li>
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content','housing','+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/icon_housing.png" class="icon">
+                <script> dl_r(20);</script>
+            </a>
+        </li>
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content','medical', '+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/Icon_health.png" class="icon">
+                <script>dl_r(21);</script>
+            </a>
+        </li>
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content','legal', '+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/icon_legal and advice.png" class="icon">
+                <script>dl_r(22);</script>
+            </a>
+        </li>
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content','study', '+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/icon_study2.png" class="icon">
+                <script>dl_r(23);</script>
+            </a>
+        </li>
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content','jobs', '+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/icon_jobs.png" class="icon">
+                <script>dl_r(24);</script>
+            </a>
+        </li>
+        <li class="list-inline-item mr-3"
+            <a href="javascript:redirect('content_modular','', '+get_local("area")+');">
+                <img src="http://local.prana.com/assets/images/icons/icon_.png" class="icon">
+                <script>dl_r(108);</script>
+            </a>
+        </li>
+    </ul>
+</div>
+<script>fill_search_type("src_in");</script>
