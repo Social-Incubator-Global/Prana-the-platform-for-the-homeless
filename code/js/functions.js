ad7e243081a50c3c9878115cba601d1f87de6491
Oscar-Tark <oscartark91@gmail.com>
2017-11-14 18:35:40 +0100

    This commit officially closes the project. If you would like to continue the project please follow the instructions in the "PROJECT TAKEOVER.TXT"

--- code/js/functions.js
+++
@@ -1,9 +0,0 @@
-function validateLogin() {
-  var usernameInput = document.getElementById('usernameInput').value;
-  var passwordInput = document.getElementById('passwordInput').value;
-
-  if(usernameInput == "" || passwordInput == "") {
-    alert("The fields are blank. Also, make them turn red.");
-    return false;
-  }
-}
