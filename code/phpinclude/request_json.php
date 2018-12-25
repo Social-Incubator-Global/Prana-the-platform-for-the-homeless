9d351ae9fcf06f7ccf4cf134870cddfd0a97bf8e
Oscar-Tark <oscartark91@gmail.com>
2018-12-25 11:12:47 +0100

    Revert "Change from prana to ticketry"
    This reverts commit 60b8470d7d987b6f9b7a751a1dc41fc5cb82ebc3.

---
+++ code/phpinclude/request_json.php
@@ -0,0 +1,27 @@
+<?php
+/*
+<Prana-deutschland. The platform for the homeless>
+    Copyright (C) <2016-2017>  <Oscar Arjun Singh Tark, Catie Carson, Nicholas Alexander Kearney, Jeremy Leresteux, Emilie Caron, Robinson Choe and all underlying members of Prana-deutschland>
+    <Original programmers: Oscar Arjun Singh Tark, Robinson Choe, Nicholas Alexander Kearney, Jeremy Leresteux>
+
+    Visitable link: www.prana-deutschland.de , for any inquiries contact at contact@prana-deutschland.de
+
+    This file is part of Prana-deutschland.
+
+    Prana-deutschland is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Prana-deutschland is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Prana-deutschland.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include('/phpinclude/sql.php');
+query_();
+?>
